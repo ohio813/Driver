08e23ea193de4b9ded1afa67f94426ebecd43def
gyf1214 <gyf_1214@163.com>
2013-10-29 15:55:16 +0800

    improve database

--- engine/database.cpp
+++ engine/database.cpp
@@ -1,7 +1,30 @@
 #include "database.h"
 
+LPCTSTR db_get_path(LPCTSTR src)
+{
+	int src_size = _tcslen(src);
+	int size = _tcslen(DATA_FILE);
+	TCHAR* ret = new TCHAR[(src_size + size + 1) * sizeof(TCHAR)];
+	int k = src_size;
+	for (const TCHAR* pos = src + src_size; pos != ret && *pos != '/' && *pos != '\\'; --pos)
+		--k;
+	for (int i = 0; i < k; ++i)
+		ret[i] = src[i];
+	for (int i = 0; i < size; ++i)
+		ret[k++] = DATA_FILE[i];
+	ret[k] = 0;
+	return ret;
+}
+
 DWORD db_query_exe_role(LPCTSTR file)
 {
-	//TODO
-	return ROLE_NULL;
+	LPCTSTR db_get_path(file);
+	FILE* input = fopen(file, "rb");
+	if (input)
+	{
+		
+	} else {
+		//TODO
+		return ROLE_NULL;
+	}
 }
--- engine/database.h
+++ engine/database.h
@@ -2,8 +2,14 @@
 #define __DATABASE
 
 #include "role.h"
+#include <string.h>
+#include <tchar.h>
+#include <stdio.h>
+
+#define DATA_FILE	"\\lc.lc"
 
 extern "C" {
+	LPCTSTR db_get_path(LPCTSTR);
 	DWORD db_query_exe_role(LPCTSTR);
 }
 
